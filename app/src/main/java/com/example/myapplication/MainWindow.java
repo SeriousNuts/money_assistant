9e8f37ad157756c4a774d69d23dc2dd8a52b9538
Yainele <tawtawker@gmail.com>
2020-11-07 21:47:39 +0700

    uraPidor9

--- app/src/main/java/com/example/myapplication/MainWindow.java
+++ app/src/main/java/com/example/myapplication/MainWindow.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,20 +33,102 @@ import java.util.ArrayList;
 
 public class MainWindow extends AppCompatActivity {
     PieChart pieChart;
-    private Object TextWatcher;
-    float valueUsers[]={40.5f,13.2f,60.7f,40.8f,20f};
-//String Users[]={s}
+    private TextView ChartName;
+    private TextView UserOne;
+    private TextView UserTwo;
+    private TextView UserThree;
+    private TextView UserFour;
+    private TextView UserFive;
+    private TextView ValueUs1;
+    private TextView ValueUs2;
+    private TextView ValueUs3;
+    private TextView ValueUs4;
+    private TextView ValueUs5;
+    String User1;
+    String User2;
+    String User3;
+    String User4;
+    String User5;
+    String Value1;
+    String Value2;
+    String Value3;
+    String Value4;
+    String Value5;
+    String pieChartName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.main_window);
-
-
-
-
+//Добавление Пользовательских Данных
+        //Имя Диаграммы
+        ChartName = (TextView) findViewById(R.id.ChartName);
+        Intent intentReceived= getIntent();
+        Bundle name=intentReceived.getExtras();
+        pieChartName=name.getString("Chart Name");
+        ChartName.setText(pieChartName);
+        Toast.makeText(MainWindow.this,pieChartName,Toast.LENGTH_SHORT).show();
+        //User1
+        UserOne=(TextView) findViewById(R.id.UserOne);
+        Intent intentOne= getIntent();
+        Bundle user1=intentOne.getExtras();
+        User1=user1.getString("User1");
+        UserOne.setText(User1);
+        //User2
+        UserTwo=(TextView) findViewById(R.id.UserTwo);
+        Intent intentTwo= getIntent();
+        Bundle user2=intentTwo.getExtras();
+        User2=user2.getString("User2");
+        UserTwo.setText(User2);
+        //User3
+        UserThree=(TextView) findViewById(R.id.UserThree);
+        Intent intentThree= getIntent();
+        Bundle user3=intentThree.getExtras();
+        User3=user3.getString("User3");
+        UserThree.setText(User3);
+        //User4
+        UserFour=(TextView) findViewById(R.id.UserFour);
+        Intent intentFour= getIntent();
+        Bundle user4=intentFour.getExtras();
+        User4=user4.getString("User4");
+        UserFour.setText(User4);
+        //User5
+        UserFive=(TextView) findViewById(R.id.UserFive);
+        Intent intentFive= getIntent();
+        Bundle user5=intentFive.getExtras();
+        User5=user5.getString("User5");
+        UserFive.setText(User5);
+        //Value1
+        ValueUs1=(TextView) findViewById(R.id.ValueUs1);
+        Intent intentValue1=getIntent();
+        Bundle value1= intentValue1.getExtras();
+        Value1=value1.getString("ValueOne");
+        ValueUs1.setText(Value1);
+        //Value2
+        ValueUs2=(TextView) findViewById(R.id.ValueUs2);
+        Intent intentValue2=getIntent();
+        Bundle value2= intentValue2.getExtras();
+        Value2=value2.getString("ValueTwo");
+        ValueUs2.setText(Value2);
+        //Value3
+        ValueUs3=(TextView) findViewById(R.id.ValueUs3);
+        Intent intentValue3=getIntent();
+        Bundle value3= intentValue3.getExtras();
+        Value3=value3.getString("ValueThree");
+        ValueUs3.setText(Value3);
+        //Value4
+        ValueUs4=(TextView) findViewById(R.id.ValueUs4);
+        Intent intentValue4=getIntent();
+        Bundle value4= intentValue4.getExtras();
+        Value4=value4.getString("ValueFour");
+        ValueUs4.setText(Value4);
+        //Value5
+        ValueUs5=(TextView) findViewById(R.id.ValueUs5);
+        Intent intentValue5=getIntent();
+        Bundle value5= intentValue5.getExtras();
+        Value5=value5.getString("ValueFive");
+        ValueUs5.setText(Value5);
+//////////////////////////////////////////////////////////
 
         setContentView(R.layout.main_window);
         pieChart= findViewById(R.id.piechart);
@@ -61,7 +145,7 @@ public class MainWindow extends AppCompatActivity {
         //бул рисовать окно посередине или нет
         pieChart.setDrawHoleEnabled(true);
         //задаем текст в центре
-        pieChart.setCenterText("Сбор денег");
+        pieChart.setCenterText(pieChartName);
         //размер текста в центре
         pieChart.setCenterTextSize(10f);
         //шрифт текста в центре
@@ -77,26 +161,24 @@ public class MainWindow extends AppCompatActivity {
         //измменить шрифт юзеров
         pieChart.setDrawEntryLabels(true);
         pieChart.setEntryLabelTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
-        setupPieChart();
 
-    }
-
-
-
-    private void setupPieChart() {
         ArrayList<PieEntry> yValues= new ArrayList<>();
-        for (int i=0; i< valueUsers.length;i++) {
-            yValues.add(new PieEntry(valueUsers[i], "users"));
+            yValues.add(new PieEntry(Float.parseFloat(Value1), User1));
+            yValues.add(new PieEntry(Float.parseFloat(Value2), User2));
+            yValues.add(new PieEntry(Float.parseFloat(Value3), User3));
+            yValues.add(new PieEntry(Float.parseFloat(Value4), User4));
+            yValues.add(new PieEntry(Float.parseFloat(Value5), User5));
 
-        }
-        PieDataSet dataSet = new PieDataSet(yValues,"Users");
+
+
+        PieDataSet dataSet = new PieDataSet(yValues,pieChartName);
         dataSet.setUsingSliceColorAsValueLineColor(true);
         dataSet.setFormSize(8f);
         dataSet.setValueLineColor(Color.DKGRAY);
         dataSet.setSliceSpace(5f);
         dataSet.setAutomaticallyDisableSliceSpacing(true);
         dataSet.setSelectionShift(8f);
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
         PieData data= new PieData((dataSet));
         data.setValueTextSize(10f);
         data.setValueTextColor(Color.DKGRAY);
@@ -104,7 +186,7 @@ public class MainWindow extends AppCompatActivity {
         pieChart.invalidate();
         pieChart.notifyDataSetChanged();
         pieChart.setData(data);
-        pieChart.animateY(2000);
+        pieChart.animateY(1500);
     }
 
 }
