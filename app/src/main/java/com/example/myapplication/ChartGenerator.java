143c25aad349db1f5f758c67d679594f2b816a50
Yainele <tawtawker@gmail.com>
2020-11-17 22:14:05 +0700

    commit for ilushka

--- app/src/main/java/com/example/myapplication/ChartGenerator.java
+++ app/src/main/java/com/example/myapplication/ChartGenerator.java
@@ -1,12 +1,16 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -21,6 +25,7 @@ import java.util.ArrayList;
 
 import static android.text.InputType.TYPE_CLASS_NUMBER;
 import static android.text.InputType.TYPE_TEXT_VARIATION_PERSON_NAME;
+import static android.view.Gravity.*;
 import static com.example.myapplication.R.id;
 import static com.example.myapplication.R.id.EnterName;
 import static com.example.myapplication.R.id.buttonAdd;
@@ -28,7 +33,6 @@ import static com.example.myapplication.R.layout;
 
 
 public class ChartGenerator extends AppCompatActivity implements View.OnClickListener{
-LinearLayout llMain;
     LinearLayout llMain2;
 ArrayList<String>NumbersofEditText = new ArrayList<>();
 ArrayList<String>EditTexts = new ArrayList<>();
@@ -37,15 +41,16 @@ ArrayList<String>EditTexts = new ArrayList<>();
 
 int i = 0;
 int d=0;
-    int x= 10;
+    int x= 50;
+    int buttonId;
+    int Checkid=0;
+    int Checkid2=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_chart_generator);
-        llMain=(LinearLayout) findViewById(id.llMain);
         llMain2=(LinearLayout) findViewById(id.llMain2);
     }
-
     private void scanCode(){
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setCaptureActivity(QRScanner.class);
@@ -86,6 +91,7 @@ int d=0;
         }
     }
 
+    @SuppressLint("ResourceType")
     @Override
     public void onClick(View v) {
 
@@ -97,19 +103,24 @@ int d=0;
                 //имя диаграммы
                 Name.putString("Chart Name",chartName.getText().toString());
                 intent1.putExtras(Name);
+                //пользователи
                 for (int z=0; z<NumbersofEditText2.size();z++) {
-
                         EditText ed2 = (EditText) findViewById(Integer.parseInt(NumbersofEditText2.get(z)));
                         EditTexts2.add(ed2.getText().toString());
+
                 }
 
                 intent1.putExtra("users",EditTexts2);
 
-
+                //значения
                for(int i = 0; i <NumbersofEditText.size();i++ ){
                    EditText ed = (EditText) findViewById(Integer.parseInt(NumbersofEditText.get(i)));
                    if(!ed.getText().toString().equals("")) {
                        EditTexts.add(ed.getText().toString());
+                   }
+                   else{
+                       Toast.makeText(ChartGenerator.this,"сумма не может быть пустой",Toast.LENGTH_SHORT).show();
+
                    }
 
                }
@@ -121,25 +132,54 @@ int d=0;
 
                 break;
             case buttonAdd:
+                Checkid2=Checkid2+x;
                 i++;
                 LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                param.gravity= Gravity.CENTER_HORIZONTAL;
+                param.gravity= RELATIVE_LAYOUT_DIRECTION;
                 final EditText editText = new EditText(this);
                 editText.setId(i);
-                editText.setHint("Сумма");
                 NumbersofEditText.add(Integer.toString(i));
+                editText.setHint("Сумма");
                 //запись id едиттекстов
                 editText.setInputType(TYPE_CLASS_NUMBER);
                 d=d+x;
-                LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                param2.gravity = Gravity.CENTER_HORIZONTAL;
+                final LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                param2.gravity = RIGHT;
                 final EditText Users = new EditText(this);
                 Users.setId(d);
+                NumbersofEditText2.add(Integer.toString(d));
                 Users.setHint("Имя");
                 Users.setInputType(TYPE_TEXT_VARIATION_PERSON_NAME);
-                NumbersofEditText2.add(Integer.toString(d));
+                buttonId++;
+                final Button deleteField= new Button(this);
+                deleteField.setId(buttonId);
+                deleteField.setText("Удалить");
                 llMain2.addView(Users, param2);
-                llMain.addView(editText, param);
+                llMain2.addView(editText, param);
+                llMain2.addView(deleteField);
+                    deleteField.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Checkid++;
+                            Checkid2++;
+                            llMain2.removeView(Users);
+                            llMain2.removeView(editText);
+                            llMain2.removeView(deleteField);
+                            for (int x=i-1; x>=0; x--)
+                            {
+                                if (x==Checkid) {
+                                NumbersofEditText.remove(x);
+                            }
+                            }
+                            for (int i=d-50; i>=0; i--)
+                            {
+                                if (i==Checkid2) {
+                                    NumbersofEditText2.remove(i);
+                                }
+                            }
+                        }
+
+                    });
 
                 break;
             case id.logout:
@@ -161,4 +201,12 @@ int d=0;
 
 
     }
+
 }
+   // ArrayList<Integer> positions = new ArrayList<Integer>();
+// positions.add(3); // add some sample values positions.add(6);
+// add some sample values positions.add(1);
+// add some sample values positions.add(2);
+// add some sample values positions.add(6);
+// for(int i=0;i<positions.size();i++) { if(positions.get(i) == 6) { positions.remove(i); } }
+// Log.i("========== After Remove ",":: "+positions.toString());
