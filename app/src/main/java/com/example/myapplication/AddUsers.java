295613873f1b186c56777f17e7d7b4a589ed04b2
Yainele <tawtawker@gmail.com>
2020-12-07 18:31:37 +0700

    added sms sending

--- app/src/main/java/com/example/myapplication/AddUsers.java
+++ app/src/main/java/com/example/myapplication/AddUsers.java
@@ -1,13 +1,21 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -15,6 +23,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -37,6 +46,7 @@ import java.util.Map;
 
 import static android.text.InputType.TYPE_CLASS_NUMBER;
 import static android.text.InputType.TYPE_TEXT_VARIATION_PERSON_NAME;
+import static android.text.InputType.TYPE_TEXT_VARIATION_PHONETIC;
 import static android.view.Gravity.RELATIVE_LAYOUT_DIRECTION;
 import static android.view.Gravity.RIGHT;
 import static com.example.myapplication.R.id.ChartName;
@@ -55,15 +65,27 @@ public class AddUsers extends Fragment {
     ArrayList<String> EditTexts = new ArrayList<>();
     ArrayList<String> NumbersofSummEditText = new ArrayList<>();
     ArrayList<String> EditTexts2 = new ArrayList<>();
+    ArrayList<String> NumbersofPhonesEditText = new ArrayList<>();
+    ArrayList<String> EditTexts3= new ArrayList<>();
     int i = 0;
     int d = 0;
     int x = 50;
+    int f=1000;
+    int g=0;
     int buttonId;
     FirebaseUser payment;
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    CheckBox notifyALL;
+    String SENT_SMS= "SENT_SMS";
+    String DELIVER_SMS= "DELIVER_SMS";
+    Intent sent_intent = new Intent(SENT_SMS);
+    Intent deliver_intent = new Intent(DELIVER_SMS);
+    PendingIntent sent_pi,deliver_pi;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // sent_pi= PendingIntent.getBroadcast(getActivity(),0,sent_intent,0);
+        //deliver_pi= PendingIntent.getBroadcast(getActivity(),0,deliver_intent,0);
 
     }
 
@@ -71,19 +93,20 @@ public class AddUsers extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View RootView = inflater.inflate(R.layout.fragment_add_users, container, false);
+        ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.SEND_SMS},1);
+        notifyALL= (CheckBox)RootView.findViewById(R.id.notifyAll);
         Button EnterBut = RootView.findViewById(EnterName);
         BottomNavigationView bottomMenu= RootView.findViewById(bottom_menu);
         bottomMenu.setOnNavigationItemSelectedListener(navListener);
-
+        sent_pi= PendingIntent.getBroadcast(getActivity(),0,sent_intent,0);
+        deliver_pi= PendingIntent.getBroadcast(getActivity(),0,deliver_intent,0);
         EnterBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText chartName = RootView.findViewById(R.id.ChartName);
+               final EditText chartName = RootView.findViewById(R.id.ChartName);
                 Intent intent1 = new Intent(getActivity(), MainWindow.class);
                 Bundle Name = new Bundle();
                 //имя диаграммы
-                Name.putString("Chart Name", chartName.getText().toString());
-                intent1.putExtras(Name);
                 //пользователи
                 for (int z = 0; z < NumbersofNameEditText.size(); z++) {
                     EditText ed2 = getView().findViewById(Integer.parseInt(NumbersofNameEditText.get(z)));
@@ -91,7 +114,6 @@ public class AddUsers extends Fragment {
                 }
 
                 intent1.putExtra("users", EditTexts2);
-
                 //значения
                 for (int i = 0; i < NumbersofSummEditText.size(); i++) {
                     EditText ed = getView().findViewById(Integer.parseInt(NumbersofSummEditText.get(i)));
@@ -137,12 +159,109 @@ public class AddUsers extends Fragment {
                 else{
                     Toast.makeText(getActivity(), "Введите имя дигарммы",Toast.LENGTH_SHORT).show();
                 }
+                        if(notifyALL.isChecked()){
+                            SmsManager smsManager = SmsManager.getDefault();
+                            for(int h=0; h<NumbersofPhonesEditText.size();h++) {
+                                EditText ed3 = RootView.findViewById(Integer.parseInt(NumbersofPhonesEditText.get(h)));
+                                if (!ed3.getText().toString().equals("")) {
+                                    EditTexts3.add(ed3.getText().toString());
+                                }
+
+                                    EditText  ed2 = RootView.findViewById(Integer.parseInt(NumbersofNameEditText.get(h)));
+                                    EditText ed = RootView.findViewById(Integer.parseInt(NumbersofSummEditText.get(h)));
+                                    smsManager.sendTextMessage(String.valueOf(EditTexts3), chartName.getText().toString(), ed2.getText().toString() + ",вы должны мне -" +ed.getText().toString()+" рублей." , sent_pi, deliver_pi);
+                                    EditTexts3.clear();
+                                Name.putString("Chart Name", chartName.getText().toString());
+                                intent1.putExtras(Name);
+                            }
+                        }
+
+
                 startActivity(intent1);
 
             }
         });
         return RootView;
     }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+
+        requireActivity().registerReceiver(sentReceiver, new IntentFilter(SENT_SMS));
+
+        requireActivity().registerReceiver(deliverReceiver, new IntentFilter(DELIVER_SMS));
+
+    }
+
+
+
+    @Override
+    public void onStop() {
+
+        super.onStop();
+
+        requireActivity().unregisterReceiver(sentReceiver);
+
+        requireActivity().unregisterReceiver(deliverReceiver);
+
+    }
+    BroadcastReceiver sentReceiver=new BroadcastReceiver() {
+
+        @Override
+
+        public void onReceive(Context context, Intent intent) {
+
+            switch (getResultCode()){
+
+                case Activity.RESULT_OK:
+
+                    Toast.makeText(context,"Sented",Toast.LENGTH_LONG).show();
+
+                    break;
+
+                default:
+
+                    Toast.makeText(context,"Error S",Toast.LENGTH_LONG).show();
+
+                    break;
+
+            }
+
+        }
+
+    };
+
+
+
+
+
+    BroadcastReceiver deliverReceiver=new BroadcastReceiver() {
+
+        @Override
+
+        public void onReceive(Context context, Intent intent) {
+
+            switch (getResultCode()){
+
+                case Activity.RESULT_OK:
+
+                    Toast.makeText(context,"Delivered",Toast.LENGTH_LONG).show();
+
+                    break;
+
+                default:
+
+                    Toast.makeText(context,"Error D",Toast.LENGTH_LONG).show();
+
+                    break;
+
+            }
+
+        }
+
+    };
 private BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -172,6 +291,9 @@ private BottomNavigationView.OnNavigationItemSelectedListener navListener= new B
                         editText.setHint("Сумма");
                         //запись id едиттекстов
                         editText.setInputType(TYPE_CLASS_NUMBER);
+
+
+
                         d = d + x;
                         final LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
                                 LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -182,19 +304,34 @@ private BottomNavigationView.OnNavigationItemSelectedListener navListener= new B
                         Users.setHint("Имя");
                         Users.setInputType(TYPE_TEXT_VARIATION_PERSON_NAME);
                         buttonId++;
+
+                        g=g+f;
+                        final LinearLayout.LayoutParams param3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT);
+                        param3.gravity = RIGHT;
+                        final EditText Phones = new EditText(getActivity());
+                        Phones.setId(g);
+                        Phones.setHint("Номер телефона");
+                        Phones.setInputType(TYPE_TEXT_VARIATION_PHONETIC);
+                        NumbersofPhonesEditText.add(Integer.toString(g));
+
                         final Button deleteField = new Button(getActivity());
                         deleteField.setId(buttonId);
                         deleteField.setText("Удалить");
                         llMain2.addView(Users, param2);
                         llMain2.addView(editText, param);
+                        llMain2.addView(Phones,param3);
                         llMain2.addView(deleteField);
+
                         deleteField.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 NumbersofNameEditText.remove(Integer.toString(Users.getId()));
                                 NumbersofSummEditText.remove(Integer.toString(editText.getId()));
+                                NumbersofPhonesEditText.remove(Integer.toString(Phones.getId()));
                                 llMain2.removeView(Users);
                                 llMain2.removeView(editText);
+                                llMain2.removeView(Phones);
                                 llMain2.removeView(deleteField);
                             }
 
